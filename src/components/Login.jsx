16b0d47a5d4e35c078ce9ee99c21f26987c4cac3
Nikitha-ms <nikithams2000@gmail.com>
2024-03-07 22:43:41 +0530

    changes -login , register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,53 @@
-
+import { useState } from "react";
 
 function Login() {
+
+  const [data, setdata] = useState({
+    username: "",
+    password: "",
+  });
+  
+  const handleChange = (e) => {
+    setdata({ ...data, [e.target.name]: e.target.value });
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  }
+
   return (
     <div>
-        <div className="login-container">
-            <div className="login-form">
-            <h1>Login</h1>
-            <form>
-                <div className="input-container">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
-                </div>
-                <div className="input-container">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+      <div className="login-container">
+        <div className="login-form">
+          <h1>Login</h1>
+          <form>
+            <div className="input-container">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={data.username}
+                onChange={handleChange}
+              />
             </div>
+            <div className="input-container">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit" onClick={handleSubmit}>Login</button>
+          </form>
         </div>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,32 +1,63 @@
+import { useState } from "react";
+
 function Register() {
+  const [data, setdata] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setdata({ ...data, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  }
+
   return (
     <div>
-        <div className="register-container">
-            <div className="register-form">
-            <h1>Register</h1>
-            <form>
-                <div className="input-container">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
-                </div>
-                <div className="input-container">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" />
-                </div>
-                <div className="input-container">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
-                </div>
-                <div className="input-container">
-                <label htmlFor="confirm-password">Confirm Password</label>
-                <input type="password" id="confirm-password" name="confirm-password" />
-                </div>
-                <button type="submit">Register</button>
-            </form>
+      <div className="register-container">
+        <div className="register-form">
+          <h1>Register</h1>
+          <form>
+            <div className="input-container">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={data.username}
+                onChange={handleChange}
+              />
             </div>
+            <div className="input-container">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="input-container">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit" onClick={handleSubmit}>
+              Register
+            </button>
+          </form>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
